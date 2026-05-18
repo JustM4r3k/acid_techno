fcb4e87fda8924256dd052e3263d767f83c4d57a
Arnas <arnas1860@gmail.com>
2026-05-18 15:12:11 +0200

    fully operational simulated acidity sensor

---
+++ acid_techno/read_acidity_node.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+import matplotlib.image as mpimg
+import numpy as np
+ 
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
+
+# Some constants
+ACIDITY_MAP_PATH = '/ws/src/acid_techno/resource/AcidityMap.jpg'
+GRID_SIZE_X = 5.0
+GRID_SIZE_Y = 5.0
+
+class ReadAcidityNode(Node):
+    acidity_map: np.ndarray
+    map_height: int
+    map_width: int
+
+    def __init__(self):
+        super().__init__('read_acidity_node')
+ 
+        self.sub = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.odom_callback,
+            10
+        )
+
+        self.pub = self.create_publisher(Float64, 'acidity', 20)
+
+        self.acidity_map = mpimg.imread(ACIDITY_MAP_PATH)
+        self.map_height, self.map_width, _ = self.acidity_map.shape
+
+        self.get_logger().info('Acidity sensor operational')
+ 
+    def odom_callback(self, msg: Odometry):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+
+        relative_x = x / (GRID_SIZE_X / 2) * (self.map_width / 2)
+        relative_y = y / (GRID_SIZE_Y / 2) * (self.map_height / 2)
+
+        if  abs(relative_x) >= self.map_width / 2 or abs(relative_y) > self.map_height / 2:
+            self.get_logger().warn('Robot out of bounds')
+            return
+        
+        pixel_x = int(relative_x + (self.map_width / 2)) 
+        pixel_y = int(relative_y + (self.map_height / 2))
+
+        brightness, _, _ = self.acidity_map[pixel_y, pixel_x]
+        pos_ph = 6 + brightness * (3 / 255)
+
+        self.get_logger().info(f'Acidity at given location ({x}, {y}) is {pos_ph}ph')
+
+        temp = Float64()
+        temp.data = pos_ph
+        self.pub.publish(temp)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ReadAcidityNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+ 
+ 
+if __name__ == '__main__':
+    main()
